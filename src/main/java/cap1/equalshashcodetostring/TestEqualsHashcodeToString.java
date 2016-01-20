3f3a1c5df879da096c07688e07964b26cecc716e
Fernando Amaral <fernandoluizjr@gmail.com>
2016-01-20 15:12:46 -0200

    adicionando classe para demonstrar o uso do metodo equals

--- src/main/java/cap1/equalshashcodetostring/TestEqualsHashcodeToString.java
+++ src/main/java/cap1/equalshashcodetostring/TestEqualsHashcodeToString.java
@@ -4,6 +4,11 @@ public class TestEqualsHashcodeToString {
   public static void main(String[] args) {
     Hippo h1 = new Hippo("Harry", 3100);
     System.out.println(h1); // calls toString method and prints Harry
+
+    //Test equals of two lions with the same id
+    Lion l1 = new Lion(1, 20, "Lion A");
+    Lion l2 = new Lion(1, 25, "Lion B");
+    System.out.println(l1.equals(l2));
   }
 }
 
@@ -19,5 +24,26 @@ class Hippo {
   @Override
   public String toString() {
     return name + "weight " + weight;
+  }
+}
+
+class Lion {
+  private int idNumber;
+  private int age;
+  private String name;
+
+  Lion(int idNumber, int age, String name) {
+    this.idNumber = idNumber;
+    this.age = age;
+    this.name = name;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    //protect against null objects returning false and be sure of the cast
+    if (!(obj instanceof Lion))
+      return false;
+    Lion otherLion = (Lion) obj;
+    return this.idNumber == otherLion.idNumber;
   }
 }
