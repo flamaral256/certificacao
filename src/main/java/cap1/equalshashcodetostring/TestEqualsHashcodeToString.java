e7b5d22bdc23d0e30491551f5d5bc84ca8c70df2
Fernando Amaral <fernandoluizjr@gmail.com>
2016-01-20 15:46:40 -0200

    adicionando classe para demonstrar o uso do metodo hashcode e equals

--- src/main/java/cap1/equalshashcodetostring/TestEqualsHashcodeToString.java
+++ src/main/java/cap1/equalshashcodetostring/TestEqualsHashcodeToString.java
@@ -9,6 +9,17 @@ public class TestEqualsHashcodeToString {
     Lion l1 = new Lion(1, 20, "Lion A");
     Lion l2 = new Lion(1, 25, "Lion B");
     System.out.println(l1.equals(l2));
+
+    //Test hashcode (group of cards) and equals (specific card)
+    Card ace1 = new Card("aces", "1");
+    Card ace2 = new Card("aces", "2");
+    Card two1 = new Card("twos", "1");
+    Card two2 = new Card("twos", "2");
+    System.out.println(ace1.hashCode() == ace2.hashCode()); //true as aces are in the same group
+    System.out.println(two1.hashCode() == two2.hashCode()); //true as twos are in the same group
+    System.out.println(ace1.hashCode() == two1.hashCode()); //false as twos and aces are in different groups
+    System.out.println(ace1.equals(ace2)); //false as aces are in the same group but are different cards
+    System.out.println(ace1.equals(ace1)); //true as aces are in the same group and also are the same card number
   }
 }
 
@@ -45,5 +56,32 @@ class Lion {
       return false;
     Lion otherLion = (Lion) obj;
     return this.idNumber == otherLion.idNumber;
+  }
+}
+
+class Card {
+  private String rank;
+  private String suit;
+
+  Card(String r, String s) {
+    if (r == null || s == null)
+      throw new IllegalArgumentException();
+    rank = r;
+    suit = s;
+  }
+
+  // Here we return a specific card inside that group
+  @Override
+  public boolean equals(Object obj) {
+    if (!(obj instanceof Card))
+      return false;
+    Card c = (Card) obj;
+    return rank.equals(c.rank) && suit.equals(c.suit);
+  }
+
+  // Here we return a category of cards (groups)
+  @Override
+  public int hashCode() {
+    return rank.hashCode();
   }
 }
