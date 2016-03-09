a65a5724c35be9f9d9fef076b5f64148cb3549e8
Fernando Amaral <fernandoluizjr@gmail.com>
2016-03-09 14:28:00 -0300

    demonstracao de generics sem bounds

--- src/main/java/cap3/generics/TestGenerics.java
+++ src/main/java/cap3/generics/TestGenerics.java
@@ -2,10 +2,25 @@ package cap3.generics;
 
 public class TestGenerics {
 	public static void main(String[] args) {
+    Glass<Juice> glass = new Glass<>(new Juice());
+    System.out.println(glass.getLiquid());
   }
 }
 
-class Glass<T> {
-    
+class Juice { @Override public String toString() {return "Juice";} }
+class Water { @Override public String toString() {return "Water";} }
+
+interface Liquid {
 }
 
+class Glass<T> {
+    private T liquid;
+
+    Glass(T liquid) {
+      this.liquid = liquid;
+    }
+
+    T getLiquid() {
+      return liquid;
+    }
+}
