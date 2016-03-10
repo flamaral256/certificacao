faa5018b2306a05e58693222ad4b42ffde47dadf
Fernando Amaral <fernandoluizjr@gmail.com>
2016-03-10 11:25:22 -0300

    demonstrando uso de generics com unbounded wildcards

--- src/main/java/cap3/generics/TestGenerics.java
+++ src/main/java/cap3/generics/TestGenerics.java
@@ -1,5 +1,8 @@
 package cap3.generics;
 
+import java.util.List;
+import java.util.ArrayList;
+
 public class TestGenerics {
 	public static void main(String[] args) {
     Glass<Juice> glassJuice = new Glass<>(new Juice()); //ok it implements Liquid
@@ -14,6 +17,11 @@ public class TestGenerics {
     //Glass<Liquid> glassLiquid = new Glass<Juice>(); //nok cuz both type inheritance from Object here
     //SuperGlass<Juice> glassPolymorphic = new ChieldGlass<Water>(); //nok cuz the types are different
     SuperGlass<Juice> glassPolymorphic = new ChieldGlass<Juice>();
+
+    Tray tray = new Tray();
+    tray.add(new WildGlass<Juice>());
+    tray.add(new WildGlass<Cake>());
+    tray.showContent();
   }
 }
 
@@ -65,4 +73,16 @@ class SuperGlass<T> {
 // this assert that both types (super and sub) will receive the same type T
 // allowing the inheritance between this types
 class ChieldGlass<T> extends SuperGlass<T> {
+}
+
+class WildGlass<T> {}
+
+class Tray {
+  private List<WildGlass<?>> trayContent = new ArrayList<>();
+  void add(WildGlass<?> glass) {//this tray can have glasses of any kind even cakes
+    trayContent.add(glass);
+  }
+  void showContent() {
+    System.out.println(trayContent);
+  }
 }
