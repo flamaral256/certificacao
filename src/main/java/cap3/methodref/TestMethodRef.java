95fe12d77d422910e1eb0a6f87299594a29365f6
Fernando Amaral <fernandoluizjr@gmail.com>
2016-05-03 11:33:28 -0300

    test removeIf

--- src/main/java/cap3/methodref/TestMethodRef.java
+++ src/main/java/cap3/methodref/TestMethodRef.java
@@ -1,6 +1,8 @@
 package cap3.methodref;
 
+import java.util.Collection;
 import java.util.Collections;
+import java.util.Arrays;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.function.Consumer;
@@ -29,6 +31,13 @@ public class TestMethodRef {
        // method reference and lambda of a constructor using generics. note how we must specify the type. diamond not work here
        Supplier<ArrayList<Integer>> methodRef5 = ArrayList<Integer>::new;
        Supplier<ArrayList<Integer>> lambda5 = () -> new ArrayList<Integer>();
+
+       // until java 7 we could remove objects from a collection that matches equals method and a specific index of a list
+       // in java 8 we have the power of remove objects passing a piece of code instead of an object or a index
+       List<Integer> listOfIntegers = new ArrayList<>(Arrays.asList(1,2,3,4,5));
+       Collection<Integer> listAsCollection = listOfIntegers; // just to enphasize that removeIf is in collection interface
+       System.out.println(listAsCollection);
+       listAsCollection.removeIf(i -> i % 2 == 0); // removeIf receive a predicate as a filter
+       System.out.println(listAsCollection);
     }
 }
-
